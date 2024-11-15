598000a18d2b7f8d9f77addb85a4fa5be8350b25
Ajay Dhungel <dhungelajay@gmail.com>
2024-11-14 19:33:51 -0500

    changes

---
+++ src/main/java/com/example/ajaydhungel/ajaydhungel/AjaydhungelApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajaydhungel.ajaydhungel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaydhungelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjaydhungelApplication.class, args);
+	}
+
+}
